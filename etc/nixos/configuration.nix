df1b858e0bc3e97e33cbe634298dcd147d88a8d6
Ahmad Jarara <ajarara@jarmac.org>
2017-04-26 19:56:25 -0400

    Add SSL, nginx hosting.

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -4,6 +4,12 @@
 
 { config, pkgs, ... }:
 
+let
+      # absolute
+      nginxRootPath = "/var/lib/nginx";
+      # relative to nginxRootPath
+      blogPath = "blog";
+in
 {
   # the choice here is to inline the hardware config and remove this import
   # or figure out how to keep this available remotely while
@@ -45,46 +51,48 @@
 
   # is it okay to listen on all interfaces?
   services.openssh.enable = true;
-  networking.firewall.allowedTCPPorts = [ 80 ];
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
   
-  services.nginx.enable = true;
-
   # one of the things that irks me a little bit is that NixOS doesn't really handle directory management.
   # granted this is intrinsically state, and Nix is a functional language, but _some_ state is necessary.
   systemd.services.nginxScaffolding = {
     description = "Idempotent directory setup";
-    wantedBy = [ "nginx.service" ];
+    requiredBy = [ "nginx.service" ];
     
     script = let
-      # absolute
-      rootPath = "/var/lib/nginx";
-      # relative to rootPath
-      blogPath = "blog";
     in ''
      
-      mkdir -p ${rootPath}
+      mkdir -p ${nginxRootPath}
       
-      chown nginx:nginx ${rootPath}
+      chown nginx:nginx ${nginxRootPath}
       # see https://en.wikipedia.org/wiki/Setuid#setuid_and_setgid_on_directories
       # or better yet: http://permissions-calculator.org/info/#setgid
-      chmod 2775 ${rootPath}
+      chmod 2775 ${nginxRootPath}
       
-      # does setgid work retroactively? then the above, while it does nothing, might become expensive.
-      mkdir -p ${rootPath}/${blogPath}
+      # does setgid work retroactively (ie recursively on the dirs
+      # already present)? then the above, while it does nothing, might
+      # become expensive.
+      mkdir -p ${nginxRootPath}/${blogPath}
     '';
+
+    # for some reason, still doesn't fire. Why?
     serviceConfig.Type = "oneshot";
-      
   };
   
-  # services.nginx.virtualHosts = {
-  #  "jarmac.org" =  {
-  #     locations."/" = {
-  #       port = 22;
-  #       root = "";
-  #     };
-  #     locations
-  #   };
-  # };
+  services.nginx = {
+    enable = true;
+    virtualHosts = {
+      "jarmac.org" = {
+        forceSSL = true;
+        enableACME = true;
+        serverAliases = [ "www.jarmac.org" ];
+        port = 443;
+        locations."/" = {
+          root = "${nginxRootPath}";
+        };
+      };
+    };
+  };
 
   # The NixOS release to be compatible with for stateful data such as databases.
   system.stateVersion = "17.03";
